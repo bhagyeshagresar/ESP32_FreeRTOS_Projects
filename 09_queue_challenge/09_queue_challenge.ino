455d3a540e86cc77e3ba47775b9360ac7413b384
bhagyeshagresar <bhagyeshagresar@gmail.com>
2024-11-05 14:57:51 +0530

    added the queue challenge

---
+++ 09_queue_challenge/09_queue_challenge.ino
@@ -0,0 +1,24 @@
+
+
+// Use only 1 core
+#if CONFIG_FREERTOS_UNICORE
+  static const BaseType_t app_cpu = 0;
+#else
+  static const BaseType_t app_cpu = 1;
+#endif
+
+
+
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
