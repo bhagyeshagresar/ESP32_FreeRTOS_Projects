7c267b0b19de4878a7a40dd9d083271efb439965
bhagyeshagresar <bhagyeshagresar@gmail.com>
2024-11-28 19:08:33 -0600

    finished the queue challenge

--- 09_queue_challenge/09_queue_challenge.ino
+++ 09_queue_challenge/09_queue_challenge.ino
@@ -14,6 +14,8 @@
 //Define the buffer length
 #define BUFFER_LEN 100
 
+static char buffer2[] = "blinked";
+
 //Configure the Pin
 static const int led_pin = LED_BUILTIN;
 
@@ -25,61 +27,67 @@ static volatile int led_delay = 300;
 
 
 //Global Handles for the two queues
-static QueueHandle_t msg_queue1;
-static QueueHandle_t msg_queue2;
+static QueueHandle_t delay_queue;
+static QueueHandle_t blinked_queue;
 
-
+//Prints any new message from the blinked_queue
+//Reads serial input from the user and echoes back
+//If there is a xxx delay in microseconds, sends it to delay_queue
 void sendDelay(void *parameter){
 
-  //Copy the message stored in Queue2 in this buffer
-  char from_queue2[BUFFER_LEN];
+  //Copy the message stored in blinked queue in this buffer
+  char from_blinked_queue[BUFFER_LEN];
 
   //Copy the message sent over serial if it contains "delay xx"
-  char to_terminal[BUFFER_LEN];
+  char from_serial[BUFFER_LEN];
   uint8_t index = 0;
   char c;
   char* p;
 
   //initial an empty buffer of chars
-  memset(from_queue2, 0, BUFFER_LEN);
+  memset(from_blinked_queue, 0, BUFFER_LEN);
+  memset(from_serial, 0, BUFFER_LEN);
 
   while(1){
 
-    //See if there is any "blinked" message in queue2. If so, print to the terminal
-    if(xQueueReceive(msg_queue2, (void*)&from_queue2, 0) == pdTRUE){
+    //See if there is any "blinked" message in blinked queue. If so, print to the terminal
+    if(xQueueReceive(blinked_queue, (void*)&from_blinked_queue, 0) == pdTRUE){
 
-      int n = strlen(from_queue2);
+      int n = strlen(from_blinked_queue);
       for(int i = 0; i < n; i++){
-        Serial.print(from_queue2[i]);
+        Serial.print(from_blinked_queue[i]);
       }
+      Serial.println();
     }
 
     //Read data from serial and echo back. Check if the serial input says "delay x". 
     //If so, copy the delay time to the to_queue1 buffer and send to to queue1
     if(Serial.available() > 0){
-
       c = Serial.read();
 
-    
       if(c == '\n' || c == '\r'){
         //we have reached the end of the string. Check for "delay x" usting strstr function
-        p = strstr(to_terminal, "delay is ");
+        p = strstr(from_serial, "delay is ");
 
         //this is a string literal. if you do p + n, you are pointing to unknown memory location
         //p = "delay is ";
 
         uint8_t n = strlen(p);
         
-        //char* sp = p;
-
         //Get the number from the substring
-        char* str = to_terminal + n;
+        char* str = from_serial + n;
         led_delay = atoi(str);
 
+        //Echo the user input back to serial terminal
+        Serial.println(from_serial);
 
-       
+        //send the number to delay_queue
+        if(xQueueSend(delay_queue, (void*)&led_delay, 10) != pdTRUE){
 
-        memset(to_terminal, 0, BUFFER_LEN);
+          Serial.println("failed to send message to queue 1");
+        }
+
+        memset(from_serial, 0, BUFFER_LEN);
         index = 0;
       
       }
@@ -87,55 +95,73 @@ void sendDelay(void *parameter){
       else{
 
         if(index < BUFFER_LEN-1){
-          to_terminal[index] = c;
+          from_serial[index] = c;
           index++;
         }
 
       }
 
-      
-
     }
-
-      //wait for 1secdelay 500
-      //vTaskDelay(1000/portTICK_PERIOD_MS);
-     
-
-
-    
-
-   
 
   }
 }
 
 
 
+//check to see if there is a new delay in the delay_queue
+//blink the LED at that rate
+//Send "blinked" everytime the LED blinks 100 times
+void blinkLED(void *parameter){
 
-// void blinkLED(void *parameter){
+  
+  int read_delay = 0;
+  int counter = 0;
 
-//   char buffer2[] = "blinked";
+  //Setup the pin
+  pinMode(LED_BUILTIN, OUTPUT);
 
-//   while(1){
+  while(1){
 
-//     //Lets block the task for 10 ticks just to make sure there is space in the queue
-//     // if(xQueueSend(msg_queue2, (void*)&buffer2, 10) != pdTRUE){
-//     //   Serial.println("Queue2 is full");
+    //check to see if there is led delay number in delay_queue
+    if(xQueueReceive(delay_queue, (void*)&read_delay, 0) == pdTRUE){
 
-//     // }
+      //update the delay 
+      led_delay = read_delay;
 
-//     // vTaskDelay(1000/portTICK_PERIOD_MS);
+      //Serial.println("Updated the LED rate");
 
-//   }
+    }
 
-// }
+    //blink the led at the current rate
+    digitalWrite(led_pin, HIGH);
+    vTaskDelay(led_delay/portTICK_PERIOD_MS);
+    digitalWrite(led_pin, LOW);
+    vTaskDelay(led_delay/portTICK_PERIOD_MS);
+
+    counter++;
+
+    if(counter == 20){
+
+      //Serial.println("Counter reached 100");
+      
+      //Lets block the task for 10 ticks just to make sure there is space in the queue
+      xQueueSend(blinked_queue, (void*)&buffer2, 10);
+      counter = 0;
+
+    }
+
+    //vTaskDelay(1000/portTICK_PERIOD_MS);
+
+  }
+
+}
 
 
 
 void setup() {
 
   //Configure the serial
-  Serial.begin(115200);
+  Serial.begin(300);
 
   //Wait for a moment so that we do not miss serial output
   vTaskDelay(1000/portTICK_PERIOD_MS);
@@ -143,8 +169,8 @@ void setup() {
   Serial.println("---FreeRTOS Queue Challenge");
 
   //Create two queues of specific length
-  msg_queue1 = xQueueCreate(msg_queue_len, BUFFER_LEN);
-  msg_queue2 = xQueueCreate(msg_queue_len, BUFFER_LEN);
+  delay_queue = xQueueCreate(msg_queue_len, sizeof(int));
+  blinked_queue = xQueueCreate(msg_queue_len, sizeof(buffer2));
 
   //start the two tasks
   xTaskCreatePinnedToCore(sendDelay,
@@ -156,17 +182,16 @@ void setup() {
                           app_cpu);
 
   
-  // xTaskCreatePinnedToCore(blinkLED,
-  //                         "Blink LED",
-  //                         1024,
-  //                         NULL,
-  //                         1,
-  //                         NULL,
-  //                         app_cpu);
+  xTaskCreatePinnedToCore(blinkLED,
+                          "Blink LED",
+                          2048,
+                          NULL,
+                          1,
+                          NULL,
+                          app_cpu);
 
+  //Delete setup and loop tasks
   vTaskDelete(NULL);
-
-
 
 
 }
